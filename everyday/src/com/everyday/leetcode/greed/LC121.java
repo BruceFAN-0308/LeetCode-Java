283eb3f389a0b37a1cbd1f4d073ce358146e646b
Bruce <bruce.fan0308@gmail.com>
2024-06-18 17:14:38 -0400

    [UPDATE] greed solution

---
+++ everyday/src/com/everyday/leetcode/greed/LC121.java
@@ -0,0 +1,27 @@
+package com.everyday.leetcode.greed;
+
+/**
+ * @description:
+ * @author: Luoxin Fan
+ * @create: 2024-06-18 17:04
+ **/
+public class LC121 {
+    public int maxProfit(int[] prices) {
+
+        int maxValue = 0;
+        int soFarMinIndex = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < prices[soFarMinIndex]) {
+                soFarMinIndex = i;
+            } else {
+                maxValue = Math.max(maxValue, prices[i] - prices[soFarMinIndex]);
+            }
+        }
+        return maxValue;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LC121().maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+}
