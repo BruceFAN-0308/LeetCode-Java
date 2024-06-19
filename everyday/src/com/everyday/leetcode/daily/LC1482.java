bd1fa8692250ec416ea2920b489631d07d946aaa
Bruce <bruce.fan0308@gmail.com>
2024-06-19 13:24:09 -0400

    [UPDATE] daily LeetCode

--- everyday/src/com/everyday/leetcode/daily/LC1482.java
+++ everyday/src/com/everyday/leetcode/daily/LC1482.java
@@ -8,44 +8,7 @@ import java.util.*;
  * @create: 2024-06-19 12:23
  **/
 public class LC1482 {
-//    public int minDays(int[] bloomDay, int m, int k) {
-//        if (m > bloomDay.length / k) {
-//            return -1;
-//        }
-//        int low = Integer.MAX_VALUE, high = 0;
-//        int length = bloomDay.length;
-//        for (int i = 0; i < length; i++) {
-//            low = Math.min(low, bloomDay[i]);
-//            high = Math.max(high, bloomDay[i]);
-//        }
-//        while (low < high) {
-//            int days = (high - low) / 2 + low;
-//            if (canMake(bloomDay, days, m, k)) {
-//                high = days;
-//            } else {
-//                low = days + 1;
-//            }
-//        }
-//        return low;
-//    }
-//
-//    public boolean canMake(int[] bloomDay, int days, int m, int k) {
-//        int bouquets = 0;
-//        int flowers = 0;
-//        int length = bloomDay.length;
-//        for (int i = 0; i < length && bouquets < m; i++) {
-//            if (bloomDay[i] <= days) {
-//                flowers++;
-//                if (flowers == k) {
-//                    bouquets++;
-//                    flowers = 0;
-//                }
-//            } else {
-//                flowers = 0;
-//            }
-//        }
-//        return bouquets >= m;
-//    }
+
 
     public int minDays(int[] bloomDay, int m, int k) {
         if (m > bloomDay.length / k) {
