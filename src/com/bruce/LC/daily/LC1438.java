f8ea64fcb9e3d36f8b5cbee18b3f19119c63b7af
Bruce <bruce.fan0308@gmail.com>
2024-06-23 12:42:47 -0400

    [UPDATE] daily leetcode

---
+++ src/com/bruce/LC/daily/LC1438.java
@@ -0,0 +1,48 @@
+package com.bruce.LC.daily;
+
+import java.util.LinkedList;
+
+public class LC1438 {
+    public int longestSubarray(int[] nums, int limit) {
+
+        LinkedList<Integer> increase = new LinkedList<>();
+        LinkedList<Integer> decrease = new LinkedList<>();
+
+        int max = 0;
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+
+            while (increase.size() > 0 && n < increase.getLast()) {
+                increase.removeLast();
+            }
+            increase.add(n);
+
+            while (decrease.size() > 0 && n > decrease.getLast()) {
+                decrease.removeLast();
+            }
+
+            decrease.add(n);
+
+            while (decrease.getFirst() - increase.getFirst() > limit) {
+                if (nums[left] == decrease.getFirst()) {
+                    decrease.removeFirst();
+                }
+                if (nums[left] == increase.getFirst()) {
+                    increase.removeFirst();
+                }
+                left++;
+            }
+
+            int size = i - left + 1;
+            max = Math.max(max, size);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LC1438().longestSubarray(new int[]{8, 2, 4, 7}, 2));
+    }
+}
