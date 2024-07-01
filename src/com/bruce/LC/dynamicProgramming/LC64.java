fb51aa7c021526e2ed0a6ac134e69dc7dcdbe271
Bruce <bruce.fan0308@gmail.com>
2024-07-01 14:35:38 -0400

    [UPDATE]

--- src/com/bruce/LC/dynamicProgramming/LC64.java
+++ src/com/bruce/LC/dynamicProgramming/LC64.java
@@ -1,30 +1,23 @@
 package com.bruce.LC.dynamicProgramming;
 
-/**
- * @description:
- * @author: Luoxin Fan
- * @create: 2024-06-20 16:46
- **/
 public class LC64 {
     public int minPathSum(int[][] grid) {
 
         int m = grid.length, n = grid[0].length;
         int[][] dp = new int[m][n];
-
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (i == 0 && j == 0) {
-                    dp[i][j] = grid[0][0];
-                } else if (i == 0 && j != 0) {
-                    dp[i][j] = dp[i][j - 1] + grid[i][j];
-                } else if (i != 0 && j == 0) {
-                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                    dp[0][0] = grid[0][0];
+                } else if (i == 0) {
+                    dp[0][j] = dp[0][j - 1] + grid[0][j];
+                } else if (j == 0) {
+                    dp[i][0] = dp[i - 1][0] + grid[i][0];
                 } else {
-                    dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
                 }
             }
         }
-
 
         return dp[m - 1][n - 1];
     }
