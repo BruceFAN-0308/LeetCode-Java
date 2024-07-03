d53c708ef6a2c56ac7a5e6e9d0cd649af2f08a47
Bruce <bruce.fan0308@gmail.com>
2024-07-03 15:21:41 -0400

    [UPDATE] binary search

---
+++ src/com/bruce/LC/binarySearch/LC81.java
@@ -0,0 +1,35 @@
+package com.bruce.LC.binarySearch;
+
+public class LC81 {
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] == nums[left] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            } else if (nums[mid] >= nums[left]) {
+                if (target > nums[mid] || target < nums[left]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (target > nums[right] || target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LC81().search(new int[]{5, 1, 3}, 3));
+    }
+}
