73e44da79b28c1c6ea472a16c8ed37391a580432
Bruce <bruce.fan0308@gmail.com>
2024-07-05 13:56:22 -0400

    [UPDATE] dp

---
+++ src/com/bruce/LC/dp/LC139.java
@@ -0,0 +1,27 @@
+package com.bruce.LC.dp;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LC139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LC139().wordBreak("leetcode", Arrays.asList("leet", "code")));
+    }
+}
